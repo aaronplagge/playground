90b45f6df1ed3f151e871f83b5f39f00acd29f15
Aaron M Plagge <aaronplagge@bsdsysadmin.us>
2023-04-09 21:16:49 +0000

    Added header to todo.py

--- python/todo/todo.py
+++ python/todo/todo.py
@@ -1,3 +1,23 @@
+"""
+This is just a simple script that I started after starting to watch the first
+few days in the udemy course "The Python Mega Course: Learn Python in 60 Days
+with 20 Apps".
+
+I decided to try creating a todo list app myself first rather
+than watching what the course instructor does.  Also didn't want to do a menu
+based script and rather an argument based script.
+
+This will run off of a sqlite database todo.db.  The table definition is:
+
+create table todo_list (
+    _id integer primary key autoincrement,
+    todo text not null,
+    date_added datatime default current_timestamp
+);
+
+"""
+
+
 from argparse import ArgumentParser, REMAINDER
 
 def todo_add(todo_item: str):
@@ -13,6 +33,7 @@ def main():
     actions = {"add": todo_add,
                "remove": todo_remove,
                "list": todo_list}
+
     parser = ArgumentParser()
     parser.add_argument("action", 
                         choices=["add", "remove", "list"], 
@@ -23,6 +44,7 @@ def main():
                         nargs=REMAINDER,
                         help="Item to add or remove")
     args = parser.parse_args()
+
     actions[args.action](" ".join(args.todo_item))
 
 
